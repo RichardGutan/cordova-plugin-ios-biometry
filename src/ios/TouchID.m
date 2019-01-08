66cf7d37d6143d8dae5679b8b72729fe746782d9
Richard Gutan <richard.gutan@capco.com>
2019-01-08 10:20:08 +0100

    a

--- src/ios/TouchID.m
+++ src/ios/TouchID.m
@@ -269,7 +269,7 @@ NSString *keychainItemServiceName;
 
   CFDataRef dataRef = NULL;
   OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef*)&dataRef);
-  NSLog(SecCopyErrorMessageString(status, NULL));
+  NSLog((__bridge NSString *)SecCopyErrorMessageString(status, NULL));
   return status == errSecInteractionNotAllowed;
 }
 
