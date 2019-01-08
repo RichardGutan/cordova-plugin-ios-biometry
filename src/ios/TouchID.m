54af52a7eb586f976a504bb82bc2bb20881855e3
Richard Gutan <richard.gutan@capco.com>
2019-01-08 10:13:18 +0100

    a

--- src/ios/TouchID.m
+++ src/ios/TouchID.m
@@ -237,7 +237,7 @@ NSString *keychainItemServiceName;
     NSDictionary *attributesToUpdate = @{
       (__bridge id)kSecValueData: [value dataUsingEncoding:NSUTF8StringEncoding],
     };
-    NSLog(@"123");
+    
     result = SecItemUpdate((__bridge CFDictionaryRef)query, (__bridge CFDictionaryRef)attributesToUpdate);
   } else {
     NSDictionary *query = @{
@@ -246,7 +246,7 @@ NSString *keychainItemServiceName;
       (__bridge id)kSecValueData: [value dataUsingEncoding:NSUTF8StringEncoding],
       (__bridge id)kSecAttrAccessControl: (__bridge id)accessControlRef
     };
-    NSLog(@"456");
+    
     result = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
   }
 
@@ -269,7 +269,7 @@ NSString *keychainItemServiceName;
 
   CFDataRef dataRef = NULL;
   OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef*)&dataRef);
-
+  NSLog(status);
   return status == errSecInteractionNotAllowed;
 }
 
